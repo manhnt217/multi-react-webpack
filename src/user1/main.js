8f79912fb35d0dca22e8b52355fabd69d82b132a
Manh Nguyen <manh.nguyen.217@gmail.com>
2019-03-21 00:18:11 +0700

    Fix some silly bug

--- src/user1/main.js
+++ src/user1/main.js
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 
 export default class AwesomeComp extends Component {
 
+  constructor(props) {
+    super(props)
+    this.message = 'Awesome component 1'
+  }
+
   render() {
-    return <div>{this.props.message}</div>;
+    return <div>{this.message}</div>;
   }
 }

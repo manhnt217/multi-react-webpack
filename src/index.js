8ea0cb3e0b1d099fafd10fc87ed85038d06b5f76
Manh Nguyen <manh.nguyen.217@gmail.com>
2019-03-21 00:11:12 +0700

    Import source code

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+
+import('./user1/main').then(
+  comp => {
+    const htmlString = ReactDOMServer.renderToString(React.createElement(comp.default, {message: 'GGMU'}, null));
+    console.log(htmlString)
+  }
+)
+
+import('./user2/main').then(
+  comp => {
+    const htmlString = ReactDOMServer.renderToString(React.createElement(comp.default, {message: 'GGMU'}, null));
+    console.log(htmlString)
+  }
+)
---
+++ src/user2/main.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+export default class AwesomeComp extends Component {
+
+  render() {
+    return <div>{this.props.message}</div>;
+  }
+}

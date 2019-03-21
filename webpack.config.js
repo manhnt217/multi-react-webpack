0f36ffc32080b64e056f2f59fb0c40a3a8df07d2
Manh Nguyen <tienmanh.nguyen@orchestranetworks.com>
2019-03-21 16:41:03 +0700

    Added 2 main pages

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@
 /* eslint-disable no-unused-vars */
 const webpack = require("webpack");
 const path = require("path");
+const FileListPlugin = require("./src/FileListPlugin");
+const WebpackCleanPlugin = require("webpack-clean-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
 
 /*
  * SplitChunksPlugin is enabled by default and replaced
@@ -27,85 +30,136 @@ const path = require("path");
 
 const UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        include: [path.resolve(__dirname, "src")],
-        loader: "babel-loader",
+const commonRules = [
+  {
+    test: /\.js$/,
+    exclude: /(node_modules|bower_components)/,
+    include: [path.resolve(__dirname, "src")],
+    loader: "babel-loader",
 
-        options: {
-          plugins: ["dynamic-import-node"],
-          presets: [
-            "@babel/preset-env",
-            "@babel/preset-react"
-          ]
-        }
-      },
+    options: {
+      plugins: ["syntax-dynamic-import", /* "dynamic-import-node" */],
+      presets: ["@babel/preset-env", "@babel/preset-react"]
+    }
+  },
+  {
+    test: /\.json$/,
+    use: [
       {
-        test: /\.json$/,
-        use: [
-          {
-            loader: 'json-loader'
-          }
-        ]
-      },
-      {
-        test: /\.css$/,
-
-        use: [
-          {
-            loader: "style-loader",
-
-            options: {
-              sourceMap: true
-            }
-          },
-          {
-            loader: "css-loader"
-          }
-        ]
+        loader: "json-loader"
       }
     ]
   },
+  {
+    test: /\.css$/,
 
-  target: 'node',
-  node: {
-    console: false,
-    global: false,
-    process: false,
-    Buffer: false,
-    __filename: false,
-    __dirname: false
-  },
+    use: [
+      {
+        loader: "style-loader",
 
-  entry: {
-    main: "./src/index.js",
-    // user1: "./src/user1/main.js",
-    // user2: "./src/user2/main.js"
-  },
-
-  output: {
-    // filename: '[name].[chunkhash].js'
-  },
-
-  mode: "development",
-
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        vendors: {
-          priority: -10,
-          test: /[\\/]node_modules[\\/]/
+        options: {
+          sourceMap: true
         }
       },
-
-      chunks: "async",
-      minChunks: 1,
-      minSize: 30000,
-      name: true
-    }
+      {
+        loader: "css-loader"
+      }
+    ]
   }
-};
+];
+
+module.exports = [
+  {
+    module: {
+      rules: commonRules
+    },
+
+    target: "node",
+    // node: {
+    //   console: false,
+    //   global: false,
+    //   process: false,
+    //   Buffer: false,
+    //   __filename: false,
+    //   __dirname: false
+    // },
+
+    entry: {
+      main: "./src/index.js"
+    },
+
+    output: {
+      // filename: '[name].[chunkhash].js'
+    },
+
+    mode: "development",
+
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          vendors: {
+            priority: -10,
+            test: /[\\/]node_modules[\\/]/
+          }
+        },
+
+        chunks: "async",
+        minChunks: 1,
+        minSize: 30000,
+        name: true
+      }
+    },
+    plugins: [
+      new WebpackCleanPlugin({
+        on: "emit",
+        path: ["./dist"]
+      }),
+
+      new FileListPlugin()
+    ]
+  },
+  {
+    module: {
+      rules: commonRules
+    },
+
+    entry: {
+      // main: "./src/index.js",
+      user1: "./src/user1/index.js",
+    },
+
+    output: {
+      // filename: '[name].[chunkhash].js'
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./src/user1/index.html",
+        filename: "./index1.html"
+      }),
+    ],
+
+    mode: "development"
+  },
+  {
+    module: {
+      rules: commonRules
+    },
+
+    entry: {
+      // main: "./src/index.js",
+      user2: "./src/user2/index.js",
+    },
+
+    output: {
+      // filename: '[name].[chunkhash].js'
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./src/user2/index.html",
+        filename: "./index2.html"
+      }),
+    ],
+
+    mode: "development"
+  }
+];
